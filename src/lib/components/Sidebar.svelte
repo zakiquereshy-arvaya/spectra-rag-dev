506e329739680cbfff132d8e7e721320fcc78684
zakiquereshy-arvaya <zakiquereshy@arvayaconsulting.com>
2026-01-11 14:18:17 -0500

    Update Sidebar.svelte
    removing (DO NOT USE) from appointments feature

--- src/lib/components/Sidebar.svelte
+++ src/lib/components/Sidebar.svelte
@@ -14,7 +14,8 @@
 	const navItems = [
 		{ href: '/', label: 'Home', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
 		{ href: '/spectra-job', label: 'Spectra Project Assistant', icon: 'M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z' },
-		{ href: '/appointments', label: 'Appointments (DO NOT USE IN DEV)', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
+		{ href: '/appointments', label: 'Appointments', icon: 'M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z' },
+		// ADD HREF FOR BILLI WHEN YOU INTEGRATE IT HERE.
 	];
 
 	function isActive(href: string): boolean {
