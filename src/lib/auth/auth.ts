5c84c865eab3690e9015b89f69f0ac51fd8c06fb
zakiquereshy-arvaya <zakiquereshy@arvayaconsulting.com>
2026-02-20 08:09:33 -0500

    auth fixes

--- src/lib/auth/auth.ts
+++ src/lib/auth/auth.ts
@@ -31,8 +31,6 @@ export const {handle} = SvelteKitAuth({
         async jwt({ token, account }) {
             // Persist the OAuth access_token to the token right after signin
             if (account) {
-                token.accessToken = account.access_token;
-                token.refreshToken = account.refresh_token;
                 token.accessTokenExpires = account.expires_at;
             }
             return token;
@@ -45,8 +43,8 @@ export const {handle} = SvelteKitAuth({
     },
     debug: true,
     logger: {
-        error(code, metadata) {
-            console.error('[AUTH][ERROR]', code, metadata);
+        error(error: Error) {
+            console.error('[AUTH][ERROR]', error);
         },
         warn(code) {
             console.warn('[AUTH][WARN]', code);
